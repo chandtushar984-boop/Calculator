a01fbc7644c61e1078708266bb4040288152c145
Tushar Chand <chandtushar984@gmail.com>
2026-08-07 21:31:31 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,6 @@
 # calculator.py
 
-def add(x, y):
-    return x + y
 
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        return "Error! Division by zero."
-    return x / y
 
 def main():
     print("Simple Calculator")
